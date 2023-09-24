5a056890b88814e02d40883f9df96c51e96372cb
tjy35279222 <tjy35279222@gmail.com>
2023-09-25 00:47:01 +0800

    add uniform selected spins and square selected

--- src/ising_mc.cpp
+++ src/ising_mc.cpp
@@ -11,7 +11,7 @@
 
 // Calculation parameters
 const int warm_up_time = 10000;
-const int calculation_time = 100000;
+const int calculation_time = 10000;
 const int choosen_num=64;
 // System parameters
 const int linear_size = 32; // Linear size
@@ -33,11 +33,20 @@ std::vector<double> M;
 std::vector<double> C;
 std::vector<double> T;
 
-std::vector<double> aC_tem(choosen_num*choosen_num);  //temporary correlation 
-std::vector<double> aC; //correlation
-std::vector<int> choosen_index;
-std::vector<double> mag(choosen_num); //magnitization for indiviual spin
+std::vector<double> random_aC_tem(choosen_num*choosen_num);  //temporary correlation 
+std::vector<double> random_aC; //correlation
+std::vector<double> uniform_aC_tem(choosen_num*choosen_num);
+std::vector<double> uniform_aC;
+std::vector<double> square_aC_tem(choosen_num*choosen_num);
+std::vector<double> square_aC;
 
+std::vector<int> random_choosen_index;
+std::vector<int> uniform_choosen_index;
+std::vector<int> square_choosen_index;
+
+std::vector<double> random_mag(choosen_num); //magnitization for random indiviual spin
+std::vector<double> uniform_mag(choosen_num); 
+std::vector<double> square_mag(choosen_num); 
 
 
 std::mt19937 rng(1234);
@@ -102,22 +111,45 @@ void calc_cij_mag(){
 		if(selected[random_site]== false){
 			selected[random_site]= true;
 			num_selected=num_selected+1;
-			choosen_index.push_back(random_site);
+			random_choosen_index.push_back(random_site);
 			// std::cout<<"selected position is "<<random_site<<std::endl;
 		}
 	}
+	//uniformly chhose
+	num_selected=0;
+	uniform_choosen_index.push_back(0);
+	while(num_selected<choosen_num){
+		num_selected=num_selected+1;
+		uniform_choosen_index.push_back(num_selected*4-1);
+		
+	}
+	//square choose
+	square_choosen_index.push_back(0);
+	for(i=0;i<sqrt(choosen_num);i++){
+		for(j=0;j<sqrt(choosen_num);j++){
+			square_choosen_index.push_back(i*linear_size+j);
+			std::cout<<"square_selected position is "<<i*linear_size+j<<std::endl;
+		}
+
+	}
+
 	warm_up(warm_up_time);
 	std::cout<<"end warm up"<<std::endl;
 	std::cout<<"mc step"<<std::endl;
+
 	for (int m = 0; m< calculation_time; m++) {
 		mc_step();
 		if (m%1000==0){
 			std::cout<<"calculating correlation for m="<<m<<std::endl;
 		}
 		for(i=0;i<choosen_num;i++){
-			mag[i]=mag[i]+spins[choosen_index[i]];
+			random_mag[i]=random_mag[i]+spins[random_choosen_index[i]];
+			uniform_mag[i]=uniform_mag[i]+spins[uniform_choosen_index[i]];
+			square_mag[i]=square_mag[i]+spins[square_choosen_index[i]];
 			for(j=0;j<choosen_num;j++){
-				aC_tem[i*choosen_num+j]=aC_tem[i*choosen_num+j]+spins[choosen_index[i]]*spins[choosen_index[j]];
+				random_aC_tem[i*choosen_num+j]=random_aC_tem[i*choosen_num+j]+spins[random_choosen_index[i]]*spins[random_choosen_index[j]];
+				uniform_aC_tem[i*choosen_num+j]=uniform_aC_tem[i*choosen_num+j]+spins[uniform_choosen_index[i]]*spins[uniform_choosen_index[j]];
+				square_aC_tem[i*choosen_num+j]=square_aC_tem[i*choosen_num+j]+spins[square_choosen_index[i]]*spins[square_choosen_index[j]];
 			}
 		}
 		// k=0;
@@ -147,6 +179,19 @@ void calc_cij_mag(){
 		}
 	}
 }
+void normalize(){
+	int i,j;
+	for (j=0;j<choosen_num;j++){
+		random_mag[j]=random_mag[j]/calculation_time;
+		uniform_mag[j]=uniform_mag[j]/calculation_time;
+		square_mag[j]=square_mag[j]/calculation_time;
+		for(i=0;i<j;i++){
+			random_aC.push_back(random_aC_tem[j*choosen_num+i]/calculation_time);
+			uniform_aC.push_back(uniform_aC_tem[j*choosen_num+i]/calculation_time);
+			square_aC.push_back(square_aC_tem[j*choosen_num+i]/calculation_time);
+		}
+	}
+}
 
 int main()
 {
@@ -156,57 +201,66 @@ int main()
 	beta = 1./temp;
 
 	initialize();
-
-
-	warm_up(warm_up_time);
 	calc_cij_mag();
+	normalize();
 
-	for (j=0;j<choosen_num;j++){
-		for(i=0;i<j;i++){
-			aC.push_back(aC_tem[j*choosen_num+i]/calculation_time);
-		}
-	}
-	for (i=0;i<choosen_num;i++){
-		mag[i]=mag[i]/calculation_time;
-	}
-	std::cout<<std::endl<<"vector aC"<<std::endl;
-	for (auto &v : aC) {
-        std::cout << v << " ";
-    }
-
-	// std::cout<<std::endl<<"selected spins"<<std::endl;
-	// for (auto &v : selected) {
+	// std::cout<<std::endl<<"vector aC"<<std::endl;
+	// for (auto &v : random_aC) {
     //     std::cout << v << " ";
     // }
 
-	std::cout<<std::endl<<"vector aC_tem"<<std::endl;
-	for (int i = 0; i < choosen_num*choosen_num; i++) {
-        std::cout << aC_tem[i] << " ";
-		if(i%(choosen_num)==choosen_num-1){
-			std::cout << std::endl;
-		}
-    }
+	// std::cout<<std::endl<<"vector aC_tem"<<std::endl;
+	// for (int i = 0; i < choosen_num*choosen_num; i++) {
+    //     std::cout << random_aC_tem[i] << " ";
+	// 	if(i%(choosen_num)==choosen_num-1){
+	// 		std::cout << std::endl;
+	// 	}
+    // }
 
-	for(int i = 0; i < choosen_num; i++){
-		std::cout << mag[i] << " ";
-	}
+	// for(int i = 0; i < choosen_num; i++){
+	// 	std::cout << random_mag[i] << " ";
+	// }
 
 	// save data
-	std::string fn = "mag_corre_file.h5" ;
+	std::string fn = "random_mag_corre_file.h5" ;
 	
-	H5::H5File file(fn,H5F_ACC_TRUNC);
-	// std::unique_ptr<H5::H5File> f;
-	// H5::Group g(f->createGroup("result1"));
-	// H5::Group g(file.createGroup("result1"));
+	H5::H5File file1(fn,H5F_ACC_TRUNC);
 
-	hsize_t l = aC.size();
-    H5::DataSet ds= H5::DataSet(file.createDataSet("c",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
-	ds.write(aC.data(),H5::PredType::NATIVE_DOUBLE);
+	hsize_t l = random_aC.size();
+    H5::DataSet ds= H5::DataSet(file1.createDataSet("c",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
+	ds.write(random_aC.data(),H5::PredType::NATIVE_DOUBLE);
 
 
-	l = mag.size();
-    ds= H5::DataSet(file.createDataSet("m",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
-	ds.write(mag.data(),H5::PredType::NATIVE_DOUBLE);
+	l = random_mag.size();
+    ds= H5::DataSet(file1.createDataSet("m",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
+	ds.write(random_mag.data(),H5::PredType::NATIVE_DOUBLE);
+
+	fn = "uniform_mag_corre_file.h5" ;
+	
+	H5::H5File file2(fn,H5F_ACC_TRUNC);
+
+	l = uniform_aC.size();
+    ds= H5::DataSet(file2.createDataSet("c",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
+	ds.write(uniform_aC.data(),H5::PredType::NATIVE_DOUBLE);
+
+
+	l = uniform_mag.size();
+    ds= H5::DataSet(file2.createDataSet("m",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
+	ds.write(uniform_mag.data(),H5::PredType::NATIVE_DOUBLE);
+
+	fn = "square_mag_corre_file.h5" ;
+	
+	H5::H5File file3(fn,H5F_ACC_TRUNC);
+
+	l = square_aC.size();
+    ds= H5::DataSet(file3.createDataSet("c",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
+	ds.write(square_aC.data(),H5::PredType::NATIVE_DOUBLE);
+
+
+	l = square_mag.size();
+    ds= H5::DataSet(file3.createDataSet("m",H5::PredType::NATIVE_DOUBLE,H5::DataSpace(1,&l)));
+	ds.write(square_mag.data(),H5::PredType::NATIVE_DOUBLE);
+
 
 
 	return 0;
